dbbac9155615323429ce51fe3528b226b0917534
David <dgkilolo23@gmail.com>
2020-03-04 17:25:16 +0300

    initila landing page.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#toggleMarket').click(function() {
+    $('.amenitiesMarket').toggle();    
+  });
+  $('#toggleStaff').click(function() {
+    $('.amenitiesStaff').toggle();    
+  });
+  $('#toggleSecurity').click(function() {
+    $('.amenitesSecurity').toggle();    
+  });
+
+
+});
